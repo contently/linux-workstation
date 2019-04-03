c66a2b622d2300e465080ef89e671a4aaca43c40
James Conant <james@conant.io>
2019-04-03 14:42:02 -0400

    initial commit

---
+++ bin/create-linux-install-image-usb.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# download linux image
+# verify usb drive plugged in, take this as argument
+# copy linux image over and sync
+# unmount usb device
---
+++ bin/create-linux-system-setup-usb.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# download chef, iwlwifi drivers, etc
+# verify usb drive plugged in, take this as argument
+# copy files over and sync
+# unmount usb device
