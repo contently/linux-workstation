c1d8fc6a3248f516c545d0eb5abcd3117e5bb9b4
James Conant <james@conant.io>
2019-04-04 10:36:17 -0400

    Add setup script
    v0.2.0

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-TBD
+Download setup [script](https://github.com/contently/linux-workstation/releases/latest/download/setup-linux-workstation.sh)
+
+Assume superuser:
+```bash
+$ su
+Password: -enter superuser password-
+```
+
+Execute script as superuser:
+```bash
+# setup-linux-workstation.sh
+```
--- bin/setup-linux-workstation.sh
+++ bin/setup-linux-workstation.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# TODO This script needs to be made available for download as an asset file? Somehow needs to be public.
+
 # TODO check if required username given, used later, and verify user exists - can't be superuser
 
 # TODO check if superuser
@@ -10,12 +12,20 @@ sed -i '/deb cdrom/d' /etc/apt/sources.list
 # download and install chefdk
 wget -P Downloads/ https://packages.chef.io/files/stable/chefdk/3.8.14/debian/9/chefdk_3.8.14-1_amd64.deb
 
-# TODO download and unzip release of linux-workstation repo
-mkdir -p Projects
+# download, unzip, and move release of linux-workstation repo
+curl -s https://api.github.com/repos/contently/linux-workstation/releases/latest \
+    | grep tarball_url \
+    | cut -d '"' -f 4 \
+    | wget -O Downloads/linux-workstation-latest.tar.gz -qi -
 
+mkdir -p Projects
+tar -xf Downloads/linux-workstation-latest.tar.gz -C Downloads
+mv Downloads/contently-linux-workstation-* Projects/linux-workstation
 
 # TODO set 'user' in attrs to given username
 
 # TODO run chef recipe
 
 # TODO reboot computer?
+
+echo "All done here!"
